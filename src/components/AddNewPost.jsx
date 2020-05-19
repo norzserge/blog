56f4552d667d7c975c9493eb6a73f00a62982847
norzserge <metelskiisa@mail.ru>
2020-05-19 09:23:14 +0300

    remove UseRef

--- src/components/AddNewPost.jsx
+++ src/components/AddNewPost.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Button from "./uikit/Button";
 import Textfield from "./uikit/Textfield";
 import Textarea from "./uikit/Textarea";
